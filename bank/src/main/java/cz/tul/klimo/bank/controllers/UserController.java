a6719b79041d16c772f1198ed475214698db95f7
jakubklimo <jakub.klimo@tul.cz>
2023-05-09 14:38:20 +0200

    add dvoufazove overeni

--- bank/src/main/java/cz/tul/klimo/bank/controllers/UserController.java
+++ bank/src/main/java/cz/tul/klimo/bank/controllers/UserController.java
@@ -17,26 +17,6 @@ public class UserController {
     @Autowired
     private UserDatabaseService userService;
 
-    @GetMapping("/index")
-    public String showLoginPage(@RequestParam(value = "userId", required = false) Integer userId, Model model){
-        if(userId != null){
-            model.addAttribute("userId", userId);
-        }
-        return "index";
-    }
-
-    @PostMapping("/index")
-    public String processLogin(@RequestParam("klientNum") String klientNum, @RequestParam("heslo") String heslo, Model model, HttpSession session){
-        User user = userService.getById(Integer.parseInt(klientNum));
-        if (user == null || !user.getHeslo().equals(heslo)) {
-            model.addAttribute("wrongLogin","Zadali jste špatné klientské číslo nebo heslo.");
-            return "index";
-        }else{
-            session.setAttribute("user", user);
-            return "redirect:/home";
-        }
-    }
-
     @GetMapping("/register")
     public String showRegisterPage(){
         return "register";
---
+++ bank/src/main/java/cz/tul/klimo/bank/functions/GenerateCode.java
@@ -0,0 +1,15 @@
+package cz.tul.klimo.bank.functions;
+
+import java.util.Random;
+
+public class GenerateCode {
+    public static String getRandomNumberString() {
+        // It will generate 6 digit random Number.
+        // from 0 to 999999
+        Random rnd = new Random();
+        int number = rnd.nextInt(999999);
+
+        // this will convert any number sequence into 6 character.
+        return String.format("%06d", number);
+    }
+}
